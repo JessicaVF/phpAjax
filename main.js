691fa4ad3e24aac6bc95f5a1adf317cee5dd2a15
JessicaVF <villarjessica@hotmail.com>
2021-07-05 15:27:33 +0200

    all gateaux,  getAllGateaux() (index)

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const myCards = document.querySelector(".myCards");
+const btnGetGateaux = document.querySelector("#getGateaux");
+
+btnGetGateaux.addEventListener("click", (event) => {
+  getAllGateaux();
+});
 
 function getAllGateaux() {
   let requete = new XMLHttpRequest();
   requete.open(
     "GET",
-    "http://localhost/humanBooster/garages/index.php?controller=gateau&task=indexTest"
+    "http://localhost/humanBooster/garages/index.php?controller=gateau&task=indexApi"
   );
 
   requete.onload = () => {
@@ -35,5 +40,27 @@ function makeCardsGateaux(tableauGateau) {
     cards += card;
 
     myCards.innerHTML = cards;
+    const btnsShowCake = document.querySelector(".showGateau");
+    btnsShowCake.addEventListener("click", (event) => {
+      showGateaux(element.id);
+    });
   });
+}
+
+function showGateaux(id) {
+  let requete = new XMLHttpRequest();
+
+  requete.open(
+    "GET",
+    `http://localhost/humanBooster/garages/index.php?controller=gateau&task=showApi&id=${id}`
+  );
+
+  requete.onload = () => {
+    let result = JSON.parse(requete.responseText);
+
+    console.log(result);
+    console.log(id);
+  };
+
+  requete.send();
 }
